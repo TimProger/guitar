86bc78812035bc5f80f345d67464de8924f313c3
TimProger <akylverentimofei@gmail.com>
2025-04-18 05:21:17 +0300

    фикс путей в sample url

--- src/modules/audio/SampleManager.ts
+++ src/modules/audio/SampleManager.ts
@@ -3,9 +3,9 @@ import { createSampleMap } from "./utils";
 type SampleMap = Record<string, string>;
 
 export const NOTES = [
-  'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 
-  'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3',
-  'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4'
+  'C2', 'D2', 'E2', 'F2', 'G2', 'B2',
+  'C3', 'D3', 'E3', 'F3', 'G3', 'B3',
+  'C4', 'D4', 'E4', 'F4', 'G4', 'B4',
 ];
 
 export class SampleManager {
